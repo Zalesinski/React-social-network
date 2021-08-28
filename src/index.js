1d000eb097a27190d8f58e580afe10dfb75d8f7c
Zalesinski <zalesinsky@gmail.com>
2021-08-28 19:09:01 +0300

    Changed router

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
 import App from "./App";
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
